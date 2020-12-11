b90384454b0f622ee24805c00f8140fc844a6f9c
Yaman <yamix2000@gmail.com>
2020-12-11 12:49:27 +0100

    Day-11 progress

--- day-11/script.py
+++ day-11/script.py
@@ -1,39 +1,12 @@
 import sys
 import itertools
 from collections import Counter
+from copy import deepcopy
 
 my_list = []
-history = []
 
 for line in sys.stdin:
     my_list.append(list(line.strip()))
-
-
-def get_adjecent(my_map, row, column):
-
-    up = my_map[row-1][column]
-    down = my_map[row+1][column]
-    left = my_map[row][column-1]
-    right = my_map[row][column-1]
-    up_left = my_map[row-1][column-1]
-    up_right= my_map[row-1][column+1]
-    down_left = my_map[row+1][column-1]
-    down_right = my_map[row+1][column+1]
-
-    if (my_map[row][column] == "L" and up != "#" and down != "#" and left != "#"
-            and right != "#" and up_left != "#" and up_right != "#"
-            and down_left != "#" and down_right != "#"):
-
-        my_map[row][column] = "#"
-    # elif
-    combs = Counter([up, down, left, right, up_left, up_right,
-                     down_left, down_right])
-
-    if combs["L"] >= 4:
-        my_map[row][column] = "L"
-
-    return my_map
-
 
 
 def adjacent(my_map, row, column):
@@ -51,14 +24,14 @@ def adjacent(my_map, row, column):
             adjacents.append(up_right)
 
 
-    if row+1 < len(my_map) - 1:
+    if row+1 <= len(my_map) - 1:
         down = my_map[row+1][column]
         adjacents.append(down)
 
         if column > 0:
             down_left = my_map[row+1][column-1]
             adjacents.append(down_left)
-        if column + 1 < len(my_map[row]) -1:
+        if column + 1 <= len(my_map[row]) -1:
             down_right = my_map[row+1][column+1]
             adjacents.append(down_right)
 
@@ -66,31 +39,39 @@ def adjacent(my_map, row, column):
         left = my_map[row][column-1]
         adjacents.append(left)
 
-    if column + 1 < len(my_map[row]) -1:
+    if column + 1 <= len(my_map[row]) -1:
         right = my_map[row][column+1]
         adjacents.append(right)
-
-
-
-    if row > 0 and column > 0:
-        up_left = my_map[row-1][column-1]
-        adjacents.append(up_left)
-
 
     return adjacents
 
 
+# while True:
+for i in range(2):
+    hej_list = deepcopy(my_list)
 
-# return (up, down, left, right, up_left, up_right, down_left, up_right)
+    for i, row in enumerate(my_list):
+        for j, column in enumerate(my_list):
 
-# print(get_adjecent(my_list, 15, 55))
+            amount_of_adj = len(adjacent(my_list, i, j))
+            total = Counter(adjacent(my_list, i, j))
 
-for i in range(len(my_list)):
-    for j in range(len(my_list[i])):
-        total = Counter(adjacent(my_list, i, j))
-        if total["L"] + total["."] == 8:
-        # history.append(adjacent(my_list, i, j))
+            print(total)
+            print("#" not in total.keys())
+            if my_list[i][j] == "L" and "#" not in total.keys():
+                hej_list[i][j] = "#"
 
+            elif my_list[i][j] == "#" and total["#"] <= 4:
+                hej_list[i][j] = "L"
+
+    if my_list == hej_list:
+        break
+
+    my_list = deepcopy(hej_list)
+
+
+
+for i in my_list:
+    print(i)
 totals = Counter(i for i in list(itertools.chain.from_iterable(my_list)))
 print(totals)
-
